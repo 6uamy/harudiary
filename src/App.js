3df6056536685d930cde3a691da8201f5d68c388
Hwang Seong Taek <hst007@naver.com>
2022-12-13 23:55:01 +0900

    chore: reset enviroment

--- src/App.js
+++ src/App.js
@@ -1,101 +1,14 @@
-import {useState} from 'react';
-import './App.css';
- 
-function Article(props){
-  return <article>
-    <h2>{props.title}</h2>
-    {props.body}
-  </article>
-}
+import React from 'react';
+import './App.css'
+import HaruTemplate from './components/HaruTemplate';
 
-function Header(props){
-  return <header>
-    <h1><a href="/" onClick={(event)=>{
-      event.preventDefault();
-      props.onChangeMode();
-    }}>{props.title}</a></h1>
-  </header>
-}
-
-function Nav(props){
-  const lis = []
-  for(let i=0; i<props.topics.length; i++){
-    let t = props.topics[i];
-    lis.push(<li key={t.id}>
-      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
-        event.preventDefault();
-        props.onChangeMode(Number(event.target.id));
-      }}>{t.title}</a>
-    </li>)
-  }
-  return <nav>
-    <ol>
-      {lis}
-    </ol>
-  </nav>
-}
-
-function Create(props){
-  return <article>
-    <h2>Create</h2>
-    <form onSubmit={event=>{
-      event.preventDefault();
-      const title = event.target.title.value;
-      const body = event.target.body.value;
-      props.onCreate(title, body);
-    }}>
-      <p><input type="text" name="title" placeholder="title"/></p>
-      <p><textarea name="body" placeholder="body"></textarea></p>
-      <p><input type="submit" value="Create"></input></p>
-    </form>
-  </article>
-}
 
 function App() {
-  const [mode, setMode] = useState('WELCOME');
-  const [id, setId] = useState(null);
-  const [nextId, setNextId] = useState(1);
-  const [topics, setTopics] = useState([
-  ]);
-  let content = null;
-  if(mode === 'WELCOME'){
-    content = <Article body="오늘 있었던 일들을 정리하며 하루를 되돌아봐요 :D"></Article>
-  } else if(mode === 'READ'){
-    let title, body = null;
-    for(let i=0; i<topics.length; i++){
-      if(topics[i].id === id){
-        title = topics[i].title;
-        body = topics[i].body;
-      }
-    }
-    content = <Article title={title} body={body}></Article>
-  } else if(mode === 'CREATE'){
-    content = <Create onCreate={(_title, _body)=>{
-      const newTopic = {id:nextId, title:_title, body:_body}
-      const newTopics = [...topics]
-      newTopics.push(newTopic);
-      setTopics(newTopics);
-      setMode('READ');
-      setId(nextId);
-      setNextId(nextId+1);
-    }}></Create>
-  }
   return (
-    <div className='App'>
-      <Header title="하루 일기" onChangeMode={()=>{
-        setMode('WELCOME');
-      }}></Header>
-      <Nav topics={topics} onChangeMode={(_id)=>{
-        setMode('READ');
-        setId(_id);
-      }}></Nav>
-      {content}
-      <input type='button' onClick={event=>{
-        event.preventDefault();
-        setMode('CREATE');
-      }} value='일기쓰기'></input>
-    </div>
+    <>
+      <HaruTemplate>하루 일기</HaruTemplate>
+    </>
   );
 }
- 
+
 export default App;
